ae0cf60c52bb7b98cfe1559824a2be0cccca43e3
sbanish <sjb111@pitt.edu>
2017-04-13 21:48:41 -0400

    finished assign4

--- assignment4.js
+++ assignment4.js
@@ -12,6 +12,29 @@
 // keyup events could be helpful to get value of field as the user types
 
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
-})();
+    // Magic!
+    console.log('Keepin\'n it clean with an external script!'); 
+
+// This takes the input
+   $('.flexsearch-input').keyup(function(event){
+     var searchInput = $(".flexsearch-input").val();
+     $(".matches").html("");
+     jsonSearch(searchInput);
+   });
+ 
+ // This will allow me to search and compare the input of the text
+   function jsonSearch(searchInput){
+     $.ajax({url:"http://www.mattbowytz.com/simple_api.json?data=all",type:"GET",dataType:"json"}).done(function(json){
+       var matches = [];
+       json.data.programming.forEach(function(results){
+         results = results.toLowerCase();
+         if(searchInput.length > 0 && results.startsWith(searchInput.toLowerCase())){
+           matches.push(results);
+           $(".matches").append("<p>" + results + "</p>");
+         }
+       });
+     })
+   }
+   
+  // just as a note - I did not do the bonus.
+ })();
